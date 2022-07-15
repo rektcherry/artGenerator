c6959c0cfc110bce16b05d7f94cc8b1d3d2c49b6
rektcherry <100161898+rektcherry@users.noreply.github.com>
2022-07-15 17:43:10 +0300

    Update builder.js

--- utils/builder.js
+++ utils/builder.js
@@ -54,7 +54,7 @@ const addMetadata = (_dna, _edition) => {
 const addAttributes = (_element) => {
   let selectedElement = _element.layer.selectedElement;
   attributesList.push({
-    trait: _element.layer.name,
+    trait_type: _element.layer.name,
     value: selectedElement.name,
   });
 };
